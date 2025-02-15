f5b82917f2570fb04335fe9276714bdbb13df77b
FelixSmlz <schmoelz.felix@gmail.com>
2025-02-15 16:35:37 +0100

    Refactor AddMenu test to remove unused imports for improved clarity

--- Client/src/components/AddMenu.test.tsx
+++ Client/src/components/AddMenu.test.tsx
@@ -1,5 +1,5 @@
+import { render, screen } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
-import { render, screen, fireEvent } from "@testing-library/react";
 import AddMenu from "./AddMenu";
 
 test("renders AddMenu component", () => {
